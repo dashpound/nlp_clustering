ae77911a93282b629a410245615e67b63ecf6663
John Kiley <johnekiley@gmail.com>
2019-05-15 22:31:56 -0500

    JK: Add unedited TA's sample code for clustering.

---
+++ economics/reduced-reuters-clustering-v001.py
@@ -0,0 +1,302 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 12 20:15:05 2019
+
+@author: phuynh
+"""
+###############################################################################
+### packages required to run code.  Make sure to install all required packages.
+###############################################################################
+import re,string
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import cosine_similarity
+
+from sklearn.manifold import MDS
+
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import os
+
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+
+import numpy as np
+
+import json_lines
+
+os.chdir(r'C:\Users\phuynh\Documents\NU\2019 Spring\453_Miller\Reuters')
+
+RANDOM_SEED=9999
+###############################################################################
+### read train and test dataframe.  Headers are ['category', 'corpus', 'dataset', 'filename', 'text']
+###############################################################################
+category={'category':[]}
+text={'text':[]}
+dataset={'dataset':[]}
+filename={'filename':[]}
+
+
+with open('reuters_train.jl', 'rb') as f:
+    for item in json_lines.reader(f):
+        category['category'].append(item['category'])
+        text['text'].append(item['text'])
+        filename['filename'].append(item['filename'])
+
+data=pd.concat([pd.DataFrame(category),pd.DataFrame(text),pd.DataFrame(dataset),
+                pd.DataFrame(filename)], axis=1)
+
+#for purpose of demo number of document selected was 1k.    
+data=data.sample(n=1000, replace=False, random_state =RANDOM_SEED)
+data=data.reset_index()
+
+
+
+###############################################################################
+### Function to process documents
+###############################################################################
+def clean_doc(doc): 
+    #split document into individual words
+    tokens=doc.split()
+    re_punc = re.compile('[%s]' % re.escape(string.punctuation))
+    # remove punctuation from each word
+    tokens = [re_punc.sub('', w) for w in tokens]
+    # remove remaining tokens that are not alphabetic
+    tokens = [word for word in tokens if word.isalpha()]
+    # filter out short tokens
+    tokens = [word for word in tokens if len(word) > 4]
+    #lowercase all words
+    tokens = [word.lower() for word in tokens]
+    # filter out stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w in stop_words]         
+    # word stemming    
+    # ps=PorterStemmer()
+    # tokens=[ps.stem(word) for word in tokens]
+    return tokens
+
+
+
+
+###############################################################################
+### Processing text into lists
+###############################################################################
+
+#create empty list to store text documents titles
+titles=[]
+
+#for loop which appends the DSI title to the titles list
+for i in range(0,len(data)):
+    temp_text=data['filename'].iloc[i]
+    titles.append(temp_text)
+
+#create empty list to store text documents
+text_body=[]
+
+#for loop which appends the text to the text_body list
+for i in range(0,len(data)):
+    temp_text=data['text'].iloc[i]
+    text_body.append(temp_text)
+
+    
+#empty list to store processed documents
+processed_text=[]
+#for loop to process the text to the processed_text list
+for i in text_body:
+    text=clean_doc(i)
+    processed_text.append(text)
+
+#Note: the processed_text is the PROCESSED list of documents read directly form 
+#the csv.  Note the list of words is separated by commas.
+
+
+#stitch back together individual words to reform body of text
+final_processed_text=[]
+
+for i in processed_text:
+    temp_DSI=' '.join(i)
+    final_processed_text.append(temp_DSI)
+
+   
+#Note: We stitched the processed text together so the TFIDF vectorizer can work.
+#Final section of code has 3 lists used.  2 of which are used for further processing.
+#(1) text_body - unused, (2) processed_text (used in W2V), 
+#(3) final_processed_text (used in TFIDF), and (4) DSI titles (used in TFIDF Matrix)
+
+
+###############################################################################
+### Sklearn TFIDF 
+###############################################################################
+#note the ngram_range will allow you to include multiple words within the TFIDF matrix
+#Call Tfidf Vectorizer
+Tfidf=TfidfVectorizer(ngram_range=(1,1))
+
+#fit the vectorizer using final processed documents.  The vectorizer requires the 
+#stiched back together document.
+
+TFIDF_matrix=Tfidf.fit_transform(final_processed_text)     
+
+#creating datafram from TFIDF Matrix
+matrix=pd.DataFrame(TFIDF_matrix.toarray(), columns=Tfidf.get_feature_names(), index=titles)
+
+
+
+###############################################################################
+### Doc2Vec
+###############################################################################
+documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(final_processed_text)]
+model = Doc2Vec(documents, vector_size=100, window=2, min_count=1, workers=4)
+
+doc2vec_df=pd.DataFrame()
+for i in range(0,len(processed_text)):
+    vector=pd.DataFrame(model.infer_vector(processed_text[i])).transpose()
+    doc2vec_df=pd.concat([doc2vec_df,vector], axis=0)
+
+doc2vec_df=doc2vec_df.reset_index()
+
+doc_titles={'title': titles}
+t=pd.DataFrame(doc_titles)
+
+doc2vec_df=pd.concat([doc2vec_df,t], axis=1)
+
+doc2vec_df=doc2vec_df.drop('index', axis=1)
+
+
+###############################################################################
+### K Means Clustering - TFIDF
+###############################################################################
+k=8
+km = KMeans(n_clusters=k, random_state =RANDOM_SEED)
+km.fit(TFIDF_matrix)
+clusters = km.labels_.tolist()
+
+
+terms = Tfidf.get_feature_names()
+Dictionary={'Doc Name':titles, 'Cluster':clusters,  'Text': final_processed_text}
+frame=pd.DataFrame(Dictionary, columns=['Cluster', 'Doc Name','Text'])
+
+
+frame=pd.concat([frame,data['category']], axis=1)
+
+frame['record']=1
+
+
+###############################################################################
+### Pivot table to see see how clusters compare to categories
+###############################################################################
+pivot=pd.pivot_table(frame, values='record', index='category',
+                     columns='Cluster', aggfunc=np.sum)
+
+
+###############################################################################
+### Top Terms per cluster
+###############################################################################
+
+print("Top terms per cluster:")
+#sort cluster centers by proximity to centroid
+order_centroids = km.cluster_centers_.argsort()[:, ::-1] 
+
+terms_dict=[]
+
+
+#save the terms for each cluster and document to dictionaries.  To be used later
+#for plotting output.
+
+#dictionary to store terms and titles
+cluster_terms={}
+cluster_title={}
+
+
+for i in range(k):
+    print("Cluster %d:" % i),
+    temp_terms=[]
+    temp_titles=[]
+    for ind in order_centroids[i, :10]:
+        print(' %s' % terms[ind])
+        terms_dict.append(terms[ind])
+        temp_terms.append(terms[ind])
+    cluster_terms[i]=temp_terms
+    
+    print("Cluster %d titles:" % i, end='')
+    temp=frame[frame['Cluster']==i]
+    for title in temp['Doc Name']:
+        print(' %s,' % title, end='')
+        temp_titles.append(title)
+    cluster_title[i]=temp_titles
+
+
+###############################################################################
+### Plotting
+###############################################################################
+
+# convert two components as we're plotting points in a two-dimensional plane
+# "precomputed" because we provide a distance matrix
+# we will also specify `random_state` so the plot is reproducible.
+
+
+mds = MDS(n_components=2, dissimilarity="precomputed", random_state=RANDOM_SEED)
+
+dist = 1 - cosine_similarity(TFIDF_matrix)
+
+pos = mds.fit_transform(dist)  # shape (n_components, n_samples)
+
+xs, ys = pos[:, 0], pos[:, 1]
+
+
+#set up colors per clusters using a dict.  number of colors must correspond to K
+cluster_colors = {0: 'black', 1: 'grey', 2: 'blue', 3: 'rosybrown', 4: 'firebrick', 
+                  5:'red', 6:'darksalmon', 7:'sienna'}
+
+
+#set up cluster names using a dict.  
+cluster_dict=cluster_title
+
+#create data frame that has the result of the MDS plus the cluster numbers and titles
+df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=range(0,len(clusters)))) 
+
+#group by cluster
+groups = df.groupby('label')
+
+fig, ax = plt.subplots(figsize=(12, 12)) # set size
+ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+
+#iterate through groups to layer the plot
+#note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
+for name, group in groups:
+    ax.plot(group.x, group.y, marker='o', linestyle='', ms=12,
+            label=cluster_dict[name], color=cluster_colors[name], 
+            mec='none')
+    ax.set_aspect('auto')
+    ax.tick_params(\
+        axis= 'x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelbottom='on')
+    ax.tick_params(\
+        axis= 'y',         # changes apply to the y-axis
+        which='both',      # both major and minor ticks are affected
+        left='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelleft='on')
+
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))      #show legend with only 1 point
+
+
+
+#The following section of code is to run the k-means algorithm on the doc2vec outputs.
+#note the differences in document clusters compared to the TFIDF matrix.
+
+
+
+
+
+
+
+
