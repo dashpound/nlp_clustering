84fcaa2c34cd2bc92ddeb82c04acbc3a5c123a36
John Kiley <johnkiley@live.com>
2019-05-18 21:38:57 -0500

    JK: Added TSNE for clustering; TFIDF finalized; testing Doc2Vec.

--- economics/clustering.py
+++ economics/clustering.py
@@ -1,18 +1,14 @@
-#!/usr/bin/env python3
 # =============================================================================
 # NLP Clustering
 # =============================================================================
-# Original code by Paul Hyuh 
+# Original code by Paul Huynh 
 # Modified by John Kiley
-#
-#
-#
-#
 # =============================================================================
 # packages required to run code.  Make sure to install all required packages.
 # =============================================================================
 
 import re,string
+import multiprocessing
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
@@ -21,7 +17,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity
 
 from sklearn.manifold import MDS
-
+from sklearn.manifold import TSNE
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
@@ -62,7 +58,7 @@ with open('all.jsonl', 'rb') as f:
 data=pd.concat([pd.DataFrame(labels),pd.DataFrame(text),pd.DataFrame(dataset),
                 pd.DataFrame(title)], axis=1)
 
-#for purpose of demo number of document selected was 1k.    
+# Sample set is small enough to leverage all of the documents 211
 data=data.sample(n=211, replace=False, random_state =RANDOM_SEED)
 data=data.reset_index()
 
@@ -113,6 +109,10 @@ print('Extract titles complete-----------------------------------------------')
 
 #%%
 
+# =============================================================================
+# Processing text into lists
+# =============================================================================
+
 #create empty list to store text documents
 text_body=[]
 
@@ -125,6 +125,10 @@ print('Extract text complete ------------------------------------------------')
     
 
 #%%    
+# =============================================================================
+# Processing text into lists
+# =============================================================================
+
 #empty list to store processed documents
 processed_text=[]
 #for loop to process the text to the processed_text list
@@ -138,8 +142,10 @@ print('Process & store text complete ----------------------------------------')
 #Note: the processed_text is the PROCESSED list of documents read directly form 
 #the csv.  Note the list of words is separated by commas.
 #%%
+# =============================================================================
+# stitch back together individual words to reform body of text
+# =============================================================================
 
-#stitch back together individual words to reform body of text
 final_processed_text=[]
 
 for i in processed_text:
@@ -173,37 +179,13 @@ matrix=pd.DataFrame(TFIDF_matrix.toarray(), columns=Tfidf.get_feature_names(), i
 
 print('Sklearn TFIDF complete -----------------------------------------------')
 
-#%%
-# =============================================================================
-# Doc2Vec
-# =============================================================================
- 
-documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(final_processed_text)]
-model = Doc2Vec(documents, vector_size=100, window=2, min_count=1, workers=4)
-
-doc2vec_df=pd.DataFrame()
-for i in range(0,len(processed_text)):
-    vector=pd.DataFrame(model.infer_vector(processed_text[i])).transpose()
-    doc2vec_df=pd.concat([doc2vec_df,vector], axis=0)
-
-doc2vec_df=doc2vec_df.reset_index()
-
-doc_titles={'title': titles}
-t=pd.DataFrame(doc_titles)
-
-doc2vec_df=pd.concat([doc2vec_df,t], axis=1)
-
-doc2vec_df=doc2vec_df.drop('index', axis=1)
-
-print('Doc2Vec Complete -----------------------------------------------------')
-
 
 #%%
 # =============================================================================
 # K Means Clustering - TFIDF
 # =============================================================================
 
-k=4
+k=2
 km = KMeans(n_clusters=k, random_state =RANDOM_SEED)
 km.fit(TFIDF_matrix)
 clusters = km.labels_.tolist()
@@ -218,7 +200,7 @@ frame=pd.concat([frame,data['labels']], axis=1)
 
 frame['record']=1
 
-print('Kmeans Clustering Complete -------------------------------------------')
+print('TF-IDF Kmeans Clustering Complete -------------------------------------------')
 
 
 #%%
@@ -231,7 +213,7 @@ pivot=pd.pivot_table(frame, values='record', index='labels',
 
 print(pivot)
 
-print('Pivot Table complete -------------------------------------------------')
+print('TF-IDF Pivot Table complete -------------------------------------------------')
 
 
 #%%
@@ -275,7 +257,7 @@ print('Identify top terms per cluster complete ------------------------------')
 
 #%%
 # =============================================================================
-# Plotting
+# TF-IDF Plotting - mds algorithm
 # =============================================================================
  
 # convert two components as we're plotting points in a two-dimensional plane
@@ -284,6 +266,7 @@ print('Identify top terms per cluster complete ------------------------------')
 
 
 mds = MDS(n_components=2, dissimilarity="precomputed", random_state=RANDOM_SEED)
+#mds = TSNE(n_components=2, metric="euclidean", random_state=RANDOM_SEED)
 
 dist = 1 - cosine_similarity(TFIDF_matrix)
 
@@ -293,7 +276,7 @@ xs, ys = pos[:, 0], pos[:, 1]
 
 
 #set up colors per clusters using a dict.  number of colors must correspond to K
-cluster_colors = {0: 'black', 1: 'grey', 2: 'blue', 3: 'rosybrown', 4: 'firebrick', 
+cluster_colors = {0: 'black', 1: 'orange', 2: 'blue', 3: 'rosybrown', 4: 'firebrick', 
                   5:'red', 6:'darksalmon', 7:'sienna'}
 
 
@@ -312,7 +295,8 @@ ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
 #iterate through groups to layer the plot
 #note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
 for name, group in groups:
-    ax.plot(group.x, group.y, marker='o', linestyle='', ms=12,
+    ax.plot(group.x, group.y,
+            marker='o', linestyle='', ms=12,
             label=cluster_dict[name], color=cluster_colors[name], 
             mec='none')
     ax.set_aspect('auto')
@@ -329,10 +313,300 @@ for name, group in groups:
         top='off',         # ticks along the top edge are off
         labelleft='on')
 
+plt.title('TF-IDF Clustering | MDS Algorithm')
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))      #show legend with only 1 point
 
 #The following section of code is to run the k-means algorithm on the doc2vec outputs.
 #note the differences in document clusters compared to the TFIDF matrix.
 
-print('Plot clusters complete -----------------------------------------------')
-print('Full run complete ----------------------------------------------------')
+print('TF-IDF | MDS | Plot clusters complete -----------------------------------------------')
+
+#%%
+# =============================================================================
+# TF-IDF Plotting - TSNE algorithm
+# =============================================================================
+ 
+# convert two components as we're plotting points in a two-dimensional plane
+# "precomputed" because we provide a distance matrix
+# we will also specify `random_state` so the plot is reproducible.
+
+
+#mds = MDS(n_components=2, dissimilarity="precomputed", random_state=RANDOM_SEED)
+mds = TSNE(n_components=2, metric="euclidean", random_state=RANDOM_SEED)
+
+dist = 1 - cosine_similarity(TFIDF_matrix)
+
+pos = mds.fit_transform(dist)  # shape (n_components, n_samples)
+
+xs, ys = pos[:, 0], pos[:, 1]
+
+
+#set up colors per clusters using a dict.  number of colors must correspond to K
+cluster_colors = {0: 'black', 1: 'orange', 2: 'blue', 3: 'rosybrown', 4: 'firebrick', 
+                  5:'red', 6:'darksalmon', 7:'sienna'}
+
+
+#set up cluster names using a dict.  
+cluster_dict=cluster_title
+
+#create data frame that has the result of the MDS plus the cluster numbers and titles
+df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=range(0,len(clusters)))) 
+
+#group by cluster
+groups = df.groupby('label')
+
+fig, ax = plt.subplots(figsize=(12, 12)) # set size
+ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+
+#iterate through groups to layer the plot
+#note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
+for name, group in groups:
+    ax.plot(group.x, group.y,
+            marker='o', linestyle='', ms=12,
+            label=cluster_dict[name], color=cluster_colors[name], 
+            mec='none')
+    ax.set_aspect('auto')
+    ax.tick_params(\
+        axis= 'x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelbottom='on')
+    ax.tick_params(\
+        axis= 'y',         # changes apply to the y-axis
+        which='both',      # both major and minor ticks are affected
+        left='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelleft='on')
+
+plt.title('TF-IDF Clustering | TSNE Algorithm')
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))      #show legend with only 1 point
+
+print('TF-IDF | TSNE | Plot clusters complete -----------------------------------------------')
+
+#%%
+# =============================================================================
+# Doc2Vec
+# =============================================================================
+
+print('\nBegin Doc2Vec Work')
+cores = multiprocessing.cpu_count()
+print("\nNumber of processor cores:", cores)
+
+documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(final_processed_text)]
+model = Doc2Vec(documents, vector_size = 51192, window = 2, 
+	min_count = 2, workers = cores, epochs = 40)
+
+doc2vec_df=pd.DataFrame()
+for i in range(0,len(processed_text)):
+    vector=pd.DataFrame(model.infer_vector(processed_text[i])).transpose()
+    doc2vec_df=pd.concat([doc2vec_df,vector], axis=0)
+
+doc2vec_df=doc2vec_df.reset_index()
+
+doc_titles={'title': titles}
+t=pd.DataFrame(doc_titles)
+
+doc2vec_df=pd.concat([doc2vec_df,t], axis=1)
+
+doc2vec_df=doc2vec_df.drop('index', axis=1)
+doc2vec_df = doc2vec_df.set_index('title')
+
+print('Doc2Vec Complete -----------------------------------------------------')
+
+#%%
+# =============================================================================
+# K Means Clustering - Doc2Vec
+# =============================================================================
+
+k=2
+km = KMeans(n_clusters=k, random_state =RANDOM_SEED)
+km.fit(doc2vec_df)
+clusters = km.labels_.tolist()
+
+Dictionary={'Doc Name':titles, 'Cluster':clusters,  'Text': final_processed_text}
+frame=pd.DataFrame(Dictionary, columns=['Cluster', 'Doc Name','Text'])
+frame=pd.concat([frame,data['labels']], axis=1)
+
+frame['record']=1
+
+print('Doc2Vec Kmeans Clustering Complete -------------------------------------------')
+#%%
+# =============================================================================
+# Doc2Vec Pivot Table
+# =============================================================================
+
+pivot=pd.pivot_table(frame, values='record', index='labels',
+                     columns='Cluster', aggfunc=np.sum)
+
+print(pivot)
+
+print('Doc2Vec Pivot Table complete -------------------------------------------------')
+
+#%%
+# =============================================================================
+# Doc2Vec Top Terms per cluster
+# =============================================================================
+
+print("Top terms per cluster:")
+#sort cluster centers by proximity to centroid
+order_centroids = km.cluster_centers_.argsort()[:, ::-1] 
+
+terms_dict=[]
+
+
+#save the terms for each cluster and document to dictionaries.  To be used later
+#for plotting output.
+
+#dictionary to store terms and titles
+cluster_terms={}
+cluster_title={}
+
+
+for i in range(k):
+    print("Cluster %d:" % i),
+    temp_terms=[]
+    temp_titles=[]
+    for ind in order_centroids[i, :10]:
+        print(' %s' % terms[ind])
+        terms_dict.append(terms[ind])
+        temp_terms.append(terms[ind])
+    cluster_terms[i]=temp_terms
+    
+    print("Cluster %d titles:" % i, end='')
+    temp=frame[frame['Cluster']==i]
+    for title in temp['Doc Name']:
+        print(' %s,' % title, end='')
+        temp_titles.append(title)
+    cluster_title[i]=temp_titles
+
+print('Identify top terms per cluster complete ------------------------------')
+
+#%%
+# =============================================================================
+# Doc2Vec Plotting - mds algorithm
+# =============================================================================
+ 
+# convert two components as we're plotting points in a two-dimensional plane
+# "precomputed" because we provide a distance matrix
+# we will also specify `random_state` so the plot is reproducible.
+
+
+mds = MDS(n_components=2, dissimilarity="precomputed", random_state=RANDOM_SEED)
+#mds = TSNE(n_components=2, metric="euclidean", random_state=RANDOM_SEED)
+
+dist = 1 - cosine_similarity(doc2vec_df)
+
+pos = mds.fit_transform(dist)  # shape (n_components, n_samples)
+
+xs, ys = pos[:, 0], pos[:, 1]
+
+
+#set up colors per clusters using a dict.  number of colors must correspond to K
+cluster_colors = {0: 'black', 1: 'orange', 2: 'blue', 3: 'rosybrown', 4: 'firebrick', 
+                  5:'red', 6:'darksalmon', 7:'sienna'}
+
+
+#set up cluster names using a dict.  
+cluster_dict=cluster_title
+
+#create data frame that has the result of the MDS plus the cluster numbers and titles
+df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=range(0,len(clusters)))) 
+
+#group by cluster
+groups = df.groupby('label')
+
+fig, ax = plt.subplots(figsize=(12, 12)) # set size
+ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+
+#iterate through groups to layer the plot
+#note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
+for name, group in groups:
+    ax.plot(group.x, group.y,
+            marker='o', linestyle='', ms=12,
+            label=cluster_dict[name], color=cluster_colors[name], 
+            mec='none')
+    ax.set_aspect('auto')
+    ax.tick_params(\
+        axis= 'x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelbottom='on')
+    ax.tick_params(\
+        axis= 'y',         # changes apply to the y-axis
+        which='both',      # both major and minor ticks are affected
+        left='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelleft='on')
+
+plt.title('Doc2Vec Clustering | MDS Algorithm')
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))      #show legend with only 1 point
+
+#The following section of code is to run the k-means algorithm on the doc2vec outputs.
+#note the differences in document clusters compared to the TFIDF matrix.
+
+print('doc2vec | MDS | Plot clusters complete -----------------------------------------------')
+
+#%%
+# =============================================================================
+# doc2Vec Plotting - mds algorithm
+# =============================================================================
+ 
+# convert two components as we're plotting points in a two-dimensional plane
+# "precomputed" because we provide a distance matrix
+# we will also specify `random_state` so the plot is reproducible.
+
+
+#mds = MDS(n_components=2, dissimilarity="precomputed", random_state=RANDOM_SEED)
+mds = TSNE(n_components=2, metric="euclidean", random_state=RANDOM_SEED)
+
+dist = 1 - cosine_similarity(doc2vec_df)
+
+pos = mds.fit_transform(dist)  # shape (n_components, n_samples)
+
+xs, ys = pos[:, 0], pos[:, 1]
+
+
+#set up colors per clusters using a dict.  number of colors must correspond to K
+cluster_colors = {0: 'black', 1: 'orange', 2: 'blue', 3: 'rosybrown', 4: 'firebrick', 
+                  5:'red', 6:'darksalmon', 7:'sienna'}
+
+
+#set up cluster names using a dict.  
+cluster_dict=cluster_title
+
+#create data frame that has the result of the MDS plus the cluster numbers and titles
+df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=range(0,len(clusters)))) 
+
+#group by cluster
+groups = df.groupby('label')
+
+fig, ax = plt.subplots(figsize=(12, 12)) # set size
+ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+
+#iterate through groups to layer the plot
+#note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
+for name, group in groups:
+    ax.plot(group.x, group.y,
+            marker='o', linestyle='', ms=12,
+            label=cluster_dict[name], color=cluster_colors[name], 
+            mec='none')
+    ax.set_aspect('auto')
+    ax.tick_params(\
+        axis= 'x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelbottom='on')
+    ax.tick_params(\
+        axis= 'y',         # changes apply to the y-axis
+        which='both',      # both major and minor ticks are affected
+        left='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelleft='on')
+
+plt.title('Doc2Vec Clustering | TSNE Algorithm')
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))      #show legend with only 1 point
+
+print('Doc2Vec | TSNE | Plot clusters complete -----------------------------------------------')
